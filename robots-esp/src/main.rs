07563c663a84c442a777cec276b8061692267f1d
Guilhem Saurel <guilhem.saurel@laas.fr>
2023-04-02 13:32:21 +0200

    esp: add serial interrupts

--- robots-esp/src/main.rs
+++ robots-esp/src/main.rs
@@ -1,16 +1,22 @@
-//! embassy hello world
-//!
-//! This is an example of running the embassy executor with multiple tasks
-//! concurrently.
+//! embassy_hello_world + serial_interrupts
 
 #![no_std]
 #![no_main]
 #![feature(type_alias_impl_trait)]
 
+use core::{cell::RefCell, fmt::Write};
+use critical_section::Mutex;
 use embassy_executor::Executor;
 use embassy_time::{Duration, Timer};
 use esp32c3_hal::{
-    clock::ClockControl, embassy, peripherals::Peripherals, prelude::*, timer::TimerGroup, Rtc,
+    clock::ClockControl,
+    embassy, interrupt,
+    peripherals::{self, Peripherals, UART0},
+    prelude::*,
+    riscv,
+    timer::TimerGroup,
+    uart::config::AtCmdConfig,
+    Cpu, Rtc, Uart,
 };
 use esp_backtrace as _;
 use static_cell::StaticCell;
@@ -32,6 +38,7 @@ async fn run2() {
 }
 
 static EXECUTOR: StaticCell<Executor> = StaticCell::new();
+static SERIAL: Mutex<RefCell<Option<Uart<UART0>>>> = Mutex::new(RefCell::new(None));
 
 #[entry]
 fn main() -> ! {
@@ -41,6 +48,7 @@ fn main() -> ! {
     let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
 
     let mut rtc = Rtc::new(peripherals.RTC_CNTL);
+    let mut serial0 = Uart::new(peripherals.UART0);
     let timer_group0 = TimerGroup::new(peripherals.TIMG0, &clocks);
     let mut wdt0 = timer_group0.wdt;
     let timer_group1 = TimerGroup::new(peripherals.TIMG1, &clocks);
@@ -52,11 +60,57 @@ fn main() -> ! {
     wdt0.disable();
     wdt1.disable();
 
+    serial0.set_at_cmd(AtCmdConfig::new(None, None, None, b'#', None));
+    serial0.set_rx_fifo_full_threshold(30);
+    serial0.listen_at_cmd();
+    serial0.listen_rx_fifo_full();
+
     embassy::init(&clocks, timer_group0.timer0);
+    critical_section::with(|cs| SERIAL.borrow_ref_mut(cs).replace(serial0));
+
+    interrupt::enable(
+        peripherals::Interrupt::UART0,
+        interrupt::Priority::Priority1,
+    )
+    .unwrap();
+    interrupt::set_kind(
+        Cpu::ProCpu,
+        interrupt::CpuInterrupt::Interrupt1, // Interrupt 1 handles priority one interrupts
+        interrupt::InterruptKind::Edge,
+    );
+
+    unsafe {
+        riscv::interrupt::enable();
+    }
 
     let executor = EXECUTOR.init(Executor::new());
     executor.run(|spawner| {
         spawner.spawn(run1()).ok();
         spawner.spawn(run2()).ok();
+    });
+}
+
+#[interrupt]
+fn UART0() {
+    critical_section::with(|cs| {
+        let mut serial = SERIAL.borrow_ref_mut(cs);
+        let serial = serial.as_mut().unwrap();
+
+        let mut cnt = 0;
+        while let nb::Result::Ok(_c) = serial.read() {
+            cnt += 1;
+        }
+        writeln!(serial, "Read {} bytes", cnt,).ok();
+
+        writeln!(
+            serial,
+            "Interrupt AT-CMD: {} RX-FIFO-FULL: {}",
+            serial.at_cmd_interrupt_set(),
+            serial.rx_fifo_full_interrupt_set(),
+        )
+        .ok();
+
+        serial.reset_at_cmd_interrupt();
+        serial.reset_rx_fifo_full_interrupt();
     });
 }
