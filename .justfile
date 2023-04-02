c2a8f33101be93080fd45f6477d3abada0125a00
Guilhem Saurel <guilhem.saurel@laas.fr>
2023-04-02 11:55:36 +0200

    esp: embassy

--- .justfile
+++ .justfile
@@ -5,9 +5,9 @@ test := "cargo test --color always"
 clippy_w := "-W clippy::pedantic -W clippy::nursery -W clippy::unwrap_used -W clippy::expect_used"
 clippy_a := "-A clippy::missing-errors-doc -A clippy::missing-panics-doc"
 clippy_args := "-- " + clippy_w + " " + clippy_a
-esp := "-p robots-esp --target=riscv32imc-unknown-none-elf"
-lib := "-p robots-lib"
-drv := "-p robots-drv"
+esp := "--package robots-esp --target=riscv32imc-unknown-none-elf"
+lib := "--package robots-lib"
+drv := "--package robots-drv"
 
 check-lib:
     {{check}} {{lib}}
@@ -32,5 +32,8 @@ test:
 
 esp:
     cargo espflash --release {{esp}} {{port}}
+
+mon:
+    cargo espflash --release {{esp}} --monitor {{port}}
 
 all: clippy-lib clippy-esp clippy-drv test
--- robots-esp/src/main.rs
+++ robots-esp/src/main.rs
@@ -1,26 +1,62 @@
+//! embassy hello world
+//!
+//! This is an example of running the embassy executor with multiple tasks
+//! concurrently.
+
 #![no_std]
 #![no_main]
+#![feature(type_alias_impl_trait)]
 
+use embassy_executor::Executor;
+use embassy_time::{Duration, Timer};
+use esp32c3_hal::{
+    clock::ClockControl, embassy, peripherals::Peripherals, prelude::*, timer::TimerGroup, Rtc,
+};
 use esp_backtrace as _;
-use esp_println::println;
-use hal::{clock::ClockControl, peripherals::Peripherals, prelude::*, timer::TimerGroup, Rtc};
+use static_cell::StaticCell;
+
+#[embassy_executor::task]
+async fn run1() {
+    loop {
+        esp_println::println!("Hello world from embassy using esp-hal-async!");
+        Timer::after(Duration::from_millis(1_000)).await;
+    }
+}
+
+#[embassy_executor::task]
+async fn run2() {
+    loop {
+        esp_println::println!("Bing!");
+        Timer::after(Duration::from_millis(5_000)).await;
+    }
+}
+
+static EXECUTOR: StaticCell<Executor> = StaticCell::new();
+
 #[entry]
 fn main() -> ! {
+    esp_println::println!("Init!");
     let peripherals = Peripherals::take();
     let system = peripherals.SYSTEM.split();
     let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
 
-    // Disable the RTC and TIMG watchdog timers
     let mut rtc = Rtc::new(peripherals.RTC_CNTL);
     let timer_group0 = TimerGroup::new(peripherals.TIMG0, &clocks);
     let mut wdt0 = timer_group0.wdt;
     let timer_group1 = TimerGroup::new(peripherals.TIMG1, &clocks);
     let mut wdt1 = timer_group1.wdt;
+
+    // Disable watchdog timers
     rtc.swd.disable();
     rtc.rwdt.disable();
     wdt0.disable();
     wdt1.disable();
-    println!("Hello world!");
 
-    loop {}
+    embassy::init(&clocks, timer_group0.timer0);
+
+    let executor = EXECUTOR.init(Executor::new());
+    executor.run(|spawner| {
+        spawner.spawn(run1()).ok();
+        spawner.spawn(run2()).ok();
+    });
 }
