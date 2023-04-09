c09cc928de3d94da6a43df319b8f528bc65b6a7f
Guilhem Saurel <guilhem.saurel@laas.fr>
2023-04-09 18:27:47 +0200

    clean drv

--- robots-drv/src/lib.rs
+++ robots-drv/src/lib.rs
@@ -1,69 +1,32 @@
 use async_channel::{unbounded, Receiver, Sender};
-use bytes::{BufMut, BytesMut};
 use futures::{
     stream::{SplitSink, SplitStream},
     SinkExt, StreamExt,
 };
 use tokio_serial::SerialStream;
-use tokio_util::codec::{Decoder, Encoder, Framed};
+use tokio_util::codec::{Decoder, Framed};
 
 use robots_lib::Cmd;
 
-#[derive(thiserror::Error, Debug)]
-pub enum Error {
-    #[error("IO error: {0}")]
-    Io(#[from] std::io::Error),
+mod codec;
+use codec::Codec;
 
-    #[error("RobotS lib error: {0}")]
-    Robots(#[from] robots_lib::Error),
+mod error;
+pub use error::Result;
 
-    #[error("Async channel SendError: {0}")]
-    SendError(#[from] async_channel::SendError<Cmd>),
-
-    #[error("Async channel RecvError: {0}")]
-    RecvError(#[from] async_channel::RecvError),
-}
-
-pub struct CmdCodec;
-
-impl Decoder for CmdCodec {
-    type Item = Cmd;
-    type Error = Error;
-
-    fn decode(&mut self, src: &mut BytesMut) -> Result<Option<Self::Item>, Self::Error> {
-        let endframe = src.as_ref().iter().position(|b| *b == 0);
-        Ok(if let Some(n) = endframe {
-            let mut cmd = src.split_to(n + 1);
-            Some(Cmd::from_vec(&mut cmd)?)
-        } else {
-            None
-        })
-    }
-}
-
-impl Encoder<Cmd> for CmdCodec {
-    type Error = Error;
-
-    fn encode(&mut self, cmd: Cmd, buf: &mut BytesMut) -> Result<(), Self::Error> {
-        let data = cmd.to_vec()?;
-        buf.reserve(data.len());
-        buf.put(data.as_slice());
-        Ok(())
-    }
-}
-
-pub struct Drv {
-    writer: SplitSink<Framed<SerialStream, CmdCodec>, Cmd>,
-    reader: SplitStream<Framed<SerialStream, CmdCodec>>,
+pub struct Driver {
+    writer: SplitSink<Framed<SerialStream, Codec>, Cmd>,
+    reader: SplitStream<Framed<SerialStream, Codec>>,
     rx_send: Sender<Cmd>,
     rx_recv: Receiver<Cmd>,
     tx_send: Sender<Cmd>,
     tx_recv: Receiver<Cmd>,
 }
 
-impl Drv {
+impl Driver {
+    #[must_use]
     pub fn new(port: SerialStream) -> Self {
-        let (writer, reader) = CmdCodec.framed(port).split();
+        let (writer, reader) = Codec.framed(port).split();
         let (rx_send, rx_recv) = unbounded();
         let (tx_send, tx_recv) = unbounded();
         Self {
@@ -76,7 +39,7 @@ impl Drv {
         }
     }
 
-    pub async fn run(&mut self) -> Result<(), Error> {
+    pub async fn run(&mut self) -> Result<()> {
         loop {
             tokio::select! {
                 Some(cmd) = self.reader.next() => {
@@ -89,10 +52,12 @@ impl Drv {
         }
     }
 
+    #[must_use]
     pub fn sender(&self) -> Sender<Cmd> {
         self.tx_send.clone()
     }
 
+    #[must_use]
     pub fn receiver(&self) -> Receiver<Cmd> {
         self.rx_recv.clone()
     }
--- robots-drv/src/main.rs
+++ robots-drv/src/main.rs
@@ -1,19 +1,19 @@
 use tokio::time::{sleep, Duration};
 use tokio_serial::SerialPortBuilderExt;
 
-use robots_drv::Drv;
+use robots_drv::{Driver, Result};
 use robots_lib::Cmd;
 
 #[tokio::main]
-async fn main() -> tokio_serial::Result<()> {
+async fn main() -> Result<()> {
     let mut uart_port = serialport::new("/dev/ttyUSB0", 115_200).open_native_async()?;
     uart_port.set_exclusive(false)?;
 
-    let mut drv = Drv::new(uart_port);
-    let sender = drv.sender();
-    let receiver = drv.receiver();
+    let mut driver = Driver::new(uart_port);
+    let sender = driver.sender();
+    let receiver = driver.receiver();
 
-    tokio::spawn(async move { drv.run().await });
+    tokio::spawn(async move { driver.run().await });
     tokio::spawn(async move {
         while let Ok(cmd) = receiver.recv().await {
             println!("received {cmd:?}");
@@ -23,7 +23,7 @@ async fn main() -> tokio_serial::Result<()> {
     for _ in 0..7 {
         for cmd in [Cmd::Get, Cmd::Ping, Cmd::Pong] {
             println!("sending {cmd:?}...");
-            sender.send(cmd).await.unwrap();
+            sender.send(cmd).await?;
             sleep(Duration::from_millis(500)).await;
         }
     }
