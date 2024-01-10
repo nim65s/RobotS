3799b4325af9e9ef16c7b2eb9f14ee1162133c8b
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-01-11 00:13:18 +0100

    stm: btn + led

--- robots-stm/src/main.rs
+++ robots-stm/src/main.rs
@@ -8,26 +8,36 @@
 
 use defmt::{error, info, panic, Format};
 use embassy_executor::Spawner;
-use embassy_futures::join::join;
-use embassy_futures::select::{select, Either};
-use embassy_stm32::gpio::{Level, Output, Speed};
-use embassy_stm32::time::Hertz;
-use embassy_stm32::usb::Driver;
-use embassy_stm32::{bind_interrupts, peripherals::USB, usb, Config};
+use embassy_futures::{
+    join::join,
+    select::{select, Either},
+};
+use embassy_stm32::{
+    bind_interrupts,
+    exti::ExtiInput,
+    gpio::{Input, Level, Output, Pull, Speed},
+    peripherals::{PA0, PB2, USB},
+    time::Hertz,
+    usb::{Driver, InterruptHandler},
+    Config,
+};
 use embassy_sync::{blocking_mutex::raw::NoopRawMutex, signal::Signal};
 use embassy_time::Timer;
-use embassy_usb::class::cdc_acm::{CdcAcmClass, State};
-use embassy_usb::driver::EndpointError;
-use embassy_usb::Builder;
+use embassy_usb::{
+    class::cdc_acm::{CdcAcmClass, State},
+    driver::EndpointError,
+    Builder,
+};
 use robots_lib::{Cmd, Vec, CMD_MAX_SIZE};
 use static_cell::make_static;
 use {defmt_rtt as _, panic_probe as _};
 
 bind_interrupts!(struct Irqs {
-    USB_LP_CAN1_RX0 => usb::InterruptHandler<USB>;
+    USB_LP_CAN1_RX0 => InterruptHandler<USB>;
 });
 
 type CmdSignal = Signal<NoopRawMutex, Cmd>;
+type LedSignal = Signal<NoopRawMutex, bool>;
 type UsbDriver = Driver<'static, USB>;
 
 #[embassy_executor::main]
@@ -49,9 +59,13 @@ async fn main(spawner: Spawner) {
         Timer::after_millis(10).await;
     }
 
+    let led = Output::new(p.PB2, Level::High, Speed::Low);
+    let btn = ExtiInput::new(Input::new(p.PA0, Pull::Down), p.EXTI0);
     let usb_driver = Driver::new(p.USB, Irqs, p.PA12, p.PA11);
+
     let recv_sig = make_static!(Signal::new());
     let send_sig = make_static!(Signal::new());
+    let led_sig = make_static!(Signal::new());
 
     info!("Go !");
     if let Err(e) = spawner.spawn(usb_task(usb_driver, recv_sig, send_sig)) {
@@ -60,8 +74,14 @@ async fn main(spawner: Spawner) {
     if let Err(e) = spawner.spawn(ping_task(send_sig)) {
         error!("ping_task error: {:?}", e);
     }
-    if let Err(e) = spawner.spawn(dispatch_task(recv_sig, send_sig)) {
+    if let Err(e) = spawner.spawn(dispatch_task(recv_sig, send_sig, led_sig)) {
         error!("ping_task error: {:?}", e);
+    }
+    if let Err(e) = spawner.spawn(led_task(led, led_sig)) {
+        error!("led_task error: {:?}", e);
+    }
+    if let Err(e) = spawner.spawn(btn_task(btn, send_sig)) {
+        error!("btn_task error: {:?}", e);
     }
 }
 
@@ -148,11 +168,34 @@ impl From<EndpointError> for Disconnected {
 }
 
 #[embassy_executor::task]
-async fn dispatch_task(recv_sig: &'static CmdSignal, send_sig: &'static CmdSignal) {
+async fn dispatch_task(
+    recv_sig: &'static CmdSignal,
+    send_sig: &'static CmdSignal,
+    led_sig: &'static LedSignal,
+) {
     loop {
         match recv_sig.wait().await {
             Cmd::Ping => send_sig.signal(Cmd::Pong),
+            Cmd::Led(state) => led_sig.signal(state),
             cmd => info!("Received {:?}", cmd),
         }
+    }
+}
+
+#[embassy_executor::task]
+async fn btn_task(mut btn: ExtiInput<'static, PA0>, send_sig: &'static CmdSignal) {
+    loop {
+        btn.wait_for_rising_edge().await;
+        send_sig.signal(Cmd::Button);
+        info!("button");
+    }
+}
+
+#[embassy_executor::task]
+async fn led_task(mut led: Output<'static, PB2>, led_sig: &'static LedSignal) {
+    loop {
+        let level = led_sig.wait().await;
+        led.set_level(level.into());
+        info!("set led level {:?}", level);
     }
 }
