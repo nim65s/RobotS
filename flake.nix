3e35774ace360b7d6a544eafd232a4b1229bde6a
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-06-06 16:52:28 +0200

    flakify

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "A devShell example";
+
+  inputs = {
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          PKG_CONFIG_PATH = "${pkgs.udev.dev}/lib/pkgconfig";
+          buildInputs = with pkgs; [
+            (rust-bin.fromRustupToolchainFile  ./rust-toolchain.toml)
+            cargo-sort
+            just
+            sass
+            tailwindcss
+          ];
+        };
+      }
+    );
+}
