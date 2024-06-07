b7117748cc233d98e5b748aae32be01e8e975fd1
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-06-07 13:48:06 +0200

    nixfmt

--- flake.nix
+++ flake.nix
@@ -1,25 +1,30 @@
 {
-  description = "A devShell example";
+  description = "Rust RobotS, toy project";
 
   inputs = {
-    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
-    flake-utils.url  = "github:numtide/flake-utils";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { nixpkgs, rust-overlay, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      nixpkgs,
+      rust-overlay,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         overlays = [ (import rust-overlay) ];
-        pkgs = import nixpkgs {
-          inherit system overlays;
-        };
+        pkgs = import nixpkgs { inherit system overlays; };
       in
       {
         devShells.default = pkgs.mkShell {
           PKG_CONFIG_PATH = "${pkgs.udev.dev}/lib/pkgconfig";
           buildInputs = with pkgs; [
-            (rust-bin.fromRustupToolchainFile  ./rust-toolchain.toml)
+            (rust-bin.fromRustupToolchainFile ./rust-toolchain.toml)
             cargo-espflash
             cargo-leptos
             cargo-sort
--- shell.nix
+++ shell.nix
@@ -1,4 +1,6 @@
-{ pkgs ? import <nixpkgs> {} }:
+{
+  pkgs ? import <nixpkgs> { },
+}:
 pkgs.mkShell rec {
   buildInputs = with pkgs; [
     clang_17
@@ -12,21 +14,17 @@ pkgs.mkShell rec {
     sass
     tailwindcss
     just
-    (python311.withPackages(ps: with ps; [pyserial]))
+    (python311.withPackages (ps: with ps; [ pyserial ]))
   ];
   RUSTC_VERSION = pkgs.lib.readFile ./rust-toolchain;
-  LIBCLANG_PATH = pkgs.lib.makeLibraryPath [
-    pkgs.llvmPackages_17.libclang.lib
-  ];
+  LIBCLANG_PATH = pkgs.lib.makeLibraryPath [ pkgs.llvmPackages_17.libclang.lib ];
   shellHook = ''
     export PATH=$PATH:''${CARGO_HOME:-~/.cargo}/bin
     export PATH=$PATH:''${RUSTUP_HOME:-~/.rustup}/toolchains/$RUSTC_VERSION-x86_64-unknown-linux-gnu/bin/
-    '';
+  '';
   #RUSTFLAGS = (builtins.map (a: ''-L ${a}/lib'') [
   #]);
-  BINDGEN_EXTRA_CLANG_ARGS = (builtins.map (a: ''-I"${a}/include"'') [
-    pkgs.glibc.dev
-  ]) ++ [
+  BINDGEN_EXTRA_CLANG_ARGS = (builtins.map (a: ''-I"${a}/include"'') [ pkgs.glibc.dev ]) ++ [
     ''-I"${pkgs.llvmPackages_17.libclang.lib}/lib/clang/${pkgs.llvmPackages_17.libclang.version}/include"''
     ''-I"${pkgs.glib.dev}/include/glib-2.0"''
     ''-I"${pkgs.glib.out}/lib/glib-2.0/include/"''
